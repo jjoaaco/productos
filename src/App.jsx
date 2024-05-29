fd0a1e229496fe5b10f1d01002e087f4f6b87eaa
joaaco <jjoaco2302@gmail.com>
2024-05-29 19:00:23 -0300

    se creo otra funcion llamada productos

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import axios from 'axios';
 import './App.css';
 
 export default class App extends Component {
-  state = {
+  constructor(props){
+    super(props);
+    this.state = {
     categorias: [],
-    categoria_id: ''
-  };
-  buscarCategorias() {
-    const url = "https://productos.ctpoba.edu.ar/api/categorias";
+    categoria_id: null,
+    productos: []
+  }
+}
+  buscarCategorias(categoria_id) {
+//  const url = "https://10.0.4.103:3000/api/categorias"
+    const url="http://productos.ctpoba/api/categorias"
+    const config ={
+    params:{ categoria_id }
+    }
     axios.get(url)
       .then((resp) => {
         this.setState({ categorias: resp.data.categorias });
@@ -18,14 +26,28 @@ export default class App extends Component {
         console.log(error);
       });
   }
-
+ buscarProductos() {
+    const url = "https://productos.ctpoba/api/productos"
+//  const url = "https://10.0.4.103:3000/api/productos"
+    axios.get(url)
+      .then((resp) => {
+        this.setState({ productos: resp.data.productos});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <div>
-        <span>APP</span>
+        <span>Categorias</span>
         <input type="button"
           value="Buscar"
           onClick={() => this.buscarCategorias()} />
+        <span>Productos</span>
+        <input type='button'
+           value="Buscar"
+           onClick={() => this.buscarProductos(this.state.categoria_id)} />
         <select
           value={this.state.categoria_id}
           onChange={(event) => this.setState({ categoria_id: event.target.value })}>
@@ -36,6 +58,10 @@ export default class App extends Component {
             </option>
           )}
         </select>
+        <h3>{this.state.categoria_id}</h3>
+        {this.statte.productos.map((prod, id) =>
+        <span key={productos.id}>{ productos.nombre }</span>
+      )}
       </div>
     );
   }
